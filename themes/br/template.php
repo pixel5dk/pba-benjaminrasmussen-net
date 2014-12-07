a25031ba0c7a4b863c553fd5fe0dd8c935094cd1
Benjamin Rasmussen <pixel5dk@users.noreply.github.com>
2014-12-07 13:44:43 +0000

    Added empty sass files and a few template files

--- themes/br/template.php
+++ themes/br/template.php
@@ -1,1 +1,16 @@
 <?php
+
+function br_preprocess_node(&$vars) {
+  if($vars['view_mode'] == 'frontpage_default') {
+    $vars['theme_hook_suggestions'][] = 'node--' . $vars['node']->type . '--frontpage-default';   
+    $vars['theme_hook_suggestions'][] = 'node--' . $vars['node']->nid . '--frontpage-default';
+  }
+  if($vars['view_mode'] == 'frontpage_breaking') {
+    $vars['theme_hook_suggestions'][] = 'node--' . $vars['node']->type . '--frontpage-breaking';   
+    $vars['theme_hook_suggestions'][] = 'node--' . $vars['node']->nid . '--frontpage-breaking';
+  }
+  if($vars['view_mode'] == 'frontpage_hot') {
+    $vars['theme_hook_suggestions'][] = 'node--' . $vars['node']->type . '--frontpage-hot';   
+    $vars['theme_hook_suggestions'][] = 'node--' . $vars['node']->nid . '--frontpage-hot';
+  }
+}
