21c8f6e7eb763c33345341a44bd862db7449c978
Benjamin Rasmussen <pixel5dk@users.noreply.github.com>
2014-12-07 21:47:43 +0000

    Basic header done

--- themes/br/templates/page.tpl.php
+++ themes/br/templates/page.tpl.php
@@ -1,5 +1,18 @@
-	<?php print $messages; ?>
-<div class="content--container">
+<div class="header__container">
+  <div class="content__container">
+    <div class="header__brand">
+        <p class="header__brand--title">Benjamin Rasmussen</p>
+        <p class="header__brand--sub">Web Developer</p>
+
+    </div>
+    <div class="header__navigation">
+    <?php print theme('links__system_main_menu', array('links' => $main_menu)); ?>
+    </div>
+  </div>
+</div>
+
+<?php print $messages; ?>
+<div class="content__container">
     <?php print render($page['content_pre']); ?>
 
     <?php print render($page['content']); ?>
