30109feced446cb0b2bcf7be55172e9a5d70f017
Benjamin Rasmussen <pixel5dk@users.noreply.github.com>
2014-12-07 17:43:37 +0000

    Added favicon and styling of frontpage elements

--- themes/br/templates/blog/node--blog-post--frontpage-default.tpl.php
+++ themes/br/templates/blog/node--blog-post--frontpage-default.tpl.php
@@ -4,17 +4,19 @@
  *
  */
 dpm($node);
+dpm($content);
 ?>
-<div class="frontpage__item--container frontpage__default--container frontpage__blog">
+<a href="<?php print $node_url; ?>">
+  <div class="frontpage__item--container frontpage__default--container frontpage__blog">
+    <div class="frontpage__item--image">
+      <?php print render($content['field_cover_image']['0']); ?>
 
-  <div class="frontpage__item--image">
-    <?php print render($content['field_cover_image']['0']); ?>
-
+    </div>
+    <div class="frontpage__item--content">
+      <div class="frontpage__item--headers">
+        <p class="frontpage__item--headers--sub">blog</p>
+        <p class="frontpage__item--headers--title"><?php print render($node->title); ?></p>
+      </div>  
+    </div>
   </div>
-  <div class="frontpage__item--content">
-    <div class="frontpage__item--headers">
-      <p class="frontpage__item--headers--sub">test></p>
-      <p class="frontpage__item--headers--title"><?php print render($node->title); ?></p>
-    </div>  
-  </div>
-</div>
+</a>
--- themes/br/templates/portfolio/node--portfolio-item--frontpage-hot.tpl.php
+++ themes/br/templates/portfolio/node--portfolio-item--frontpage-hot.tpl.php
@@ -4,6 +4,17 @@
  *
  */
 ?>
-<div class="frontpage__item--container frontpage__hot--container frontpage__portfolio">
-<?php print render($content['field_image_portfolio']['0']); ?>
-</div>
+<a href="<?php print $node_url; ?>">
+  <div class="frontpage__item--container frontpage__hot--container frontpage__portfolio">
+    <div class="frontpage__item--image">
+      <?php print render($content['field_image_portfolio']['0']); ?>
+
+    </div>
+    <div class="frontpage__item--content">
+      <div class="frontpage__item--headers">
+        <p class="frontpage__item--headers--sub">blog</p>
+        <p class="frontpage__item--headers--title"><?php print render($node->title); ?></p>
+      </div>  
+    </div>
+  </div>
+</a>
