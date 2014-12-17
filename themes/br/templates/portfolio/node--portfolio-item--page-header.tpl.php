b0a2d527e7cd33bec603c42f23dd1c403b27183a
Benjamin Rasmussen <pixel5dk@users.noreply.github.com>
2014-12-17 14:58:25 +0000

    Finished styling and creating the portfolio item page

--- themes/br/templates/portfolio/node--portfolio-item--page-header.tpl.php
+++ themes/br/templates/portfolio/node--portfolio-item--page-header.tpl.php
@@ -1,5 +1,9 @@
-<?php 
-  dpm($content);
-?>
-
-test
+<div class="slider">
+  <?php 
+    $i = 0;
+    foreach ($content['field_image_portfolio']['#items'] as $item) {
+      print render($content['field_image_portfolio'][$i]);
+      $i++;
+    }
+  ?>
+</div>
